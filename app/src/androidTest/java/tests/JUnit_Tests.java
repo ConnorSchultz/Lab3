9a092d53a5bec395a281bb904deec7f841719b7e
ConnorSchultz <connorschultz13@gmail.com>
2016-04-19 13:10:51 -0700

    First Commit

---
+++ app/src/androidTest/java/tests/JUnit_Tests.java
@@ -0,0 +1,32 @@
+package tests;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+import com.example.csschult.lab3.MainActivity;
+import com.example.csschult.lab3.R;
+
+import org.w3c.dom.Text;
+
+/**
+ * Created by csschult on 4/19/2016.
+ */
+public class JUnit_Tests extends ActivityInstrumentationTestCase2<MainActivity> {
+
+
+    MainActivity mainActivity;
+
+    public JUnit_Tests() {
+
+
+        super(MainActivity.class);
+    }
+
+    public void test_first(){
+        mainActivity = getActivity();
+        TextView textView = (TextView)mainActivity.findViewById(R.id.tv1);
+            String tester = textView.getText().toString();
+        assertEquals("Bye World", tester );
+
+    }
+}
